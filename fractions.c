107ecbb91f62eee97b51ece06917db9bdc57e054
wwweert123 <puachengxuan.fp@gmail.com>
2023-09-05 20:06:16 +0800

    add fractions question

---
+++ fractions.c
@@ -0,0 +1,14 @@
+/**
+ * CS1010 Semester 1 AY23/24
+ * Bonus question By Frederick
+ *
+ * @file: fractions.c
+ * @author: XXX (Group YYY)
+ */
+
+#include "cs1010.h"
+
+int main()
+{
+    // TODO: find the number of steps required to make the two fractions equal
+}
